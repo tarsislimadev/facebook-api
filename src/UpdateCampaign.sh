f96593bd5026783605782b4564a8df079c8f89f9
Tarsis <br.tmvdl@gmail.com>
2023-09-15 15:32:28 -0300

    Adding API script files

---
+++ src/UpdateCampaign.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/sh
+
+# inputs
+
+. .env
+
+path="UpdateCampaign"
+
+datetime=$( date +%Y%m%d%H%M%S )
+
+# runner
+
+resp=$( bash get.sh "${path}" "param1=1" | jq )
+
+# outputs
+
+bash create.sh "${path}" "${datetime}" "datetime" "${datetime}"
+
+bash create.sh "${path}" "${datetime}" "resp" "${resp}"
