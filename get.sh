ca71b9752d8ba488891130167ce429a99428b7ef
Tarsis <br.tmvdl@gmail.com>
2023-09-16 16:18:03 -0300

    Updating scripts file

--- get.sh
+++ get.sh
@@ -4,10 +4,6 @@
 
 PATH="${1}"
 
-# "oauth/access_token"
-
 QUERY="${2}"
-
-# client_id=${APP_ID}&redirect_uri=${URL}&client_secret=${APP_SECRET}&code=${AUTHORIZATION_CODE}
 
 curl -X GET -sL "https://graph.facebook.com/${API_VERSION}/${PATH}?${QUERY}"
