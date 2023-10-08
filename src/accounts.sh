4b501d25179110d0b84a256b2bf4a29bebb0cb88
Tarsis <br.tmvdl@gmail.com>
2023-10-08 19:36:55 -0300

    Adding Page Access Tokens script file

---
+++ src/accounts.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/sh
+
+# https://developers.facebook.com/docs/facebook-login/guides/access-tokens
+
+# inputs
+
+. .env
+
+. datetime.sh
+
+path="${USER_ID}/accounts"
+
+# runner
+
+resp=$( bash get.sh "${path}" "access_token=${ACCESS_TOKEN}" )
+
+# outputs
+
+bash create.sh "${path}" "${datetime}" "resp.json" "${resp}"
